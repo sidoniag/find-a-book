e4de28e1d98721c83b93d92955440e08482e5d92
idonia <sidoniag@gmail.com>
2020-10-10 12:20:36 -0500

    update server.js auth.js files

--- server/server.js
+++ server/server.js
@@ -1,20 +1,35 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+// const routes = require('./routes');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create a new Apollos server and pass in our schema
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-app.use(express.urlencoded({ extended: true }));
+// integrate our Apollo server with the Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '../client/build')));
+// }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
