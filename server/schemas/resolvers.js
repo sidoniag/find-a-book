687f019e2144d74bc1f4ca3011b0639e1f8a3b66
idonia <sidoniag@gmail.com>
2020-10-11 15:09:58 -0500

    resolved server issue

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -10,33 +10,16 @@ const resolvers = {
             const userData = await User.findOne({ _id: context.user._id })
                 .select('-__v -password')
                 // .populate('thoughts')
-                .populate('books')};
+                // .populate('book')};
 
             return userData;
-        },
+        }
     
     //     throw new AuthenticationError('Not logged in');
     // },
+        },
+    },
 
-    // user: async (parent, { username }) => {
-    //         return User.findOne({ username })
-    //             .select('-__v -password')
-    //             .populate('friends')
-    //             .populate('books');
-    // },
-},
-    //     // books: async (parent, { query }) => {
-    //     //     const params = books ? { guery } : {};
-    //     //     return Book.find(params).sort({ createdAt: -1 });
-    //     // },
-    //     book: async () => {
-    //         return Book.find()
-    //         .populate('book');
-    //     },
-    //     // books: async (parent, { _id }) => {
-    //     //     return Book.findOne({ _id });
-    //     // },
-    // },
     Mutation: {
         addUser: async (parent, args) => {
             const user = await User.create(args);
@@ -44,22 +27,22 @@ const resolvers = {
 
             return { token, user };
         },
-        login: async (parent, { email, password }) => {
-            const user = await User.findOne({ email });
+        // login: async (parent, { email, password }) => {
+        //     const user = await User.findOne({ email });
 
-            if(!user) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+        //     if(!user) {
+        //         throw new AuthenticationError('Incorrect credentials');
+        //     }
 
-            const correctPw = await user.isCorrectPassword(password);
+        //     const correctPw = await user.isCorrectPassword(password);
 
-            if (!correctPw) {
-                throw new AuthenticationError('Incorrect credentials');
-            }
+        //     if (!correctPw) {
+        //         throw new AuthenticationError('Incorrect credentials');
+        //     }
 
-            const token = signToken(user);
-            return { token, user };
-        },
+        //     const token = signToken(user);
+        //     return { token, user };
+        // },
         // saveBook: async(parent,{ bookId, book }) => {
         //     const book = await Book.findOneAndUpdate(
         //         {_id: bookId },
@@ -69,6 +52,7 @@ const resolvers = {
         //     return updatedBook;
         // }
     }
+
 };
 
 module.exports = resolvers;
