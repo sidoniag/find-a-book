45a413aeb28ea1ac950039020edf8e1267ddcf12
idonia <sidoniag@gmail.com>
2020-10-30 20:49:27 -0500

    happy to deploy

--- server/server.js
+++ server/server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-// import ApolloServer
+// // import ApolloServer
 
-// const routes = require('./routes');
-
-// import typeDefs and resolvers
+// // import typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
@@ -19,13 +17,13 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-// integrate our Apollo server with the Express application as middleware
+// // integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// // if we're in production, serve client/build as static assets
+// // // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
